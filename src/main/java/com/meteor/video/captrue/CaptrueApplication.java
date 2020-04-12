226e15721d7875fa190bf4e2379ad73df36cff0c
lvjiapeng <lv1093964643@163.com>
2020-04-13 00:43:17 +0800

    init.

---
+++ src/main/java/com/meteor/video/captrue/CaptrueApplication.java
@@ -0,0 +1,13 @@
+package com.meteor.video.captrue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptrueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaptrueApplication.class, args);
+    }
+
+}
