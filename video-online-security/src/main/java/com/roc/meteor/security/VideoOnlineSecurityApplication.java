8fa8117180411716af539cbcf8f236a80218836f
jiapeng <wb-ljp573881@autonavi.com>
2020-04-16 13:49:50 +0800

    Initialization module.

---
+++ video-online-security/src/main/java/com/roc/meteor/security/VideoOnlineSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.roc.meteor.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoOnlineSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoOnlineSecurityApplication.class, args);
+    }
+
+}
---
+++ video-online-security/src/test/java/com/roc/meteor/security/VideoOnlineSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roc.meteor.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoOnlineSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
